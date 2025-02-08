e85dc9c39d6a9f72ad9ae3dbdb8d1764a2d72eed
Melissa0o0 <158860782+Melissa0o0@users.noreply.github.com>
2025-02-08 22:45:22 +0100

    weather data update 2....

--- scr/script.js
+++ scr/script.js
@@ -2,17 +2,47 @@ function UpdateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-  console.log(response.data);
-  cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = Math.round(temperature);
-}
+  let moodElement = document.querySelector("#mood");
+  let humidityElement = document.querySelector("#humidity");
+  let windspeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.date.time * 1000);
+  let emojiElement = document.querySelector("#emoji");
 
+  console.log(response.data);
+
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  moodElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  emojiElement.innerHTML = `<img  src="${response.data.condition.icon_url}" class="wetter-app-emoji" />`;
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
 function CitySearch(city) {
   let apiKey = "937eb44cb68833ao5cdt10179cf40b38";
-  let apiUrl =
-    " https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey} ";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(UpdateWeather);
-  console.log(CitySearch());
 }
 
 function SearchSubmit(event) {
