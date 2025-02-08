7da9759fc09b4d9c86d0f84137509134fd36778f
Melissa0o0 <158860782+Melissa0o0@users.noreply.github.com>
2025-02-08 16:11:18 +0100

    Api Integration

--- scr/script.js
+++ scr/script.js
@@ -1,3 +1,19 @@
+function UpdateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function CitySearch(city) {
+  let apiKey = "937eb44cb68833ao5cdt10179cf40b38";
+  let apiUrl =
+    " https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey} ";
+  axios.get(apiUrl).then(UpdateWeather);
+}
+
 function SearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
